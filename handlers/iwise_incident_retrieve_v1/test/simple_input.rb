6f502b1f09cc752c2d7892ca388a416c55fe33c8
rehmc <chad.rehm@kineticdata.com>
2019-06-18 09:21:07 -0500

    initial commit

---
+++ handlers/iwise_incident_retrieve_v1/test/simple_input.rb
@@ -0,0 +1,16 @@
+{
+  'info' => {
+    'endpoint_address' => '',
+    'endpoint_port' => '',
+    'authentication_username' => '',
+    'authentication_password' => '',
+    'authentication_transaction_server' => '',
+    'authentication_server_port' => '',
+    'authentication_account' => '',
+    'authentication_session' => '',
+    'authentication_group' => ''
+  },
+  'parameters' => {
+    'incident_id' => 'PMX/AS'
+  }
+}
